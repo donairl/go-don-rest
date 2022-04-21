67037b00c51824fbcfb3fbb964e7637d4b978f40
Donny Airlangga <donny.airlangga@gmail.com>
2022-04-21 16:32:05 +0700

    new

--- handler/Api.go
+++ handler/Api.go
@@ -18,16 +18,23 @@ func RegisterApi(e *echo.Echo, dbGlobal *gorm.DB) {
 	db = dbGlobal
 	g.GET("", adminSite)
 	g.GET("/fake", fake)
+	g.GET("/product", product)
 }
 
 func fake(c echo.Context) error {
 	myproduct := models.Product{Name: "Shampo A", Code: "10020440", Weight: 21.2, Unit: "pcs"}
 	db.Create(&myproduct)
+	myproduct1 := models.Product{Name: "Shoap Body", Code: "10020441", Weight: 18, Unit: "pcs"}
+	db.Create(&myproduct1)
+
 	return c.String(http.StatusOK, "Fake data created !\n")
 }
 
 func product(c echo.Context) error {
+	var products []models.Product
+	db.Find(&products)
 
+	return c.JSON(http.StatusOK, products)
 }
 
 func adminSite(c echo.Context) error {
